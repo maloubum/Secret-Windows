e90f2eab22b2d4416b58d4544f8c4540b36608e5
maloubum <mail@maloubumbum.com>
2022-05-19 14:06:17 +0200

    map button toggle
    hey you know what happened

---
+++ Assets/Scenes/Change Scene with button/Maptoggle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maptoggle : MonoBehaviour
+{
+    public GameObject mapbutton; 
+    public void toggle() {
+        mapbutton.SetActive(true);
+        }
+
+
+}
