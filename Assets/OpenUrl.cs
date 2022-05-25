fb60c26d87915eb3a6392322f9471c76956a80cd
maloubum <mail@maloubumbum.com>
2022-05-25 20:36:50 +0200

    updates
    it opens in safe mode, due to a scripts being faulty,

---
+++ Assets/OpenUrl.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://secretwindows.mozellosite.com/home/?fbclid=IwAR3I36W4AC6s8QPOkrm6s2p6X33rjBay3oWYc_DY90Kya7o-6Z-5pymRmaI");
+
+
+
+
+    }
